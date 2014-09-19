771a6953dc8d4a8c58db3d236f14b4e852ddc01e
FrDH <info@frebsite.nl>
2014-09-19 09:35:09 +0200

    new version

--- Guardfile
+++ Guardfile
@@ -1,11 +1,10 @@
 # Guardfile for the jQuery respontent plugin javascript files.
 # This minifies the non-minified .js-file.
 
-# For some reason, uglify only seems to work if the input and output is the same file.
-# Therefor, we need to copy the contents from the original file to the minified file (using concat) before it can be minified.
-guard :concat, type: "js", files: %w(jquery.respontent), input_dir: "src/js", output: "src/js/jquery.respontent.min"
+# Concatenate the contents of all files into jquery.respontent.min.js
+guard :concat, type: "js", files: %w(jquery.respontent media/jquery.respontent.iframes media/jquery.respontent.images media/jquery.respontent.tables), input_dir: "src/js", output: "src/js/jquery.respontent.min"
 
-# Minify the file
+# Minify jquery.respontent.min.js
 guard 'uglify', :destination_file => "src/js/jquery.respontent.min.js" do
   watch ('src/js/jquery.respontent.min.js')
 end
--- src/js/jquery.respontent.js
+++ src/js/jquery.respontent.js
@@ -1,5 +1,5 @@
 /*	
- * jQuery respontent v1.0.0
+ * jQuery respontent v1.2.0
  * @requires jQuery 1.5.0 or later
  *
  * respontent.frebsite.nl
@@ -7,9 +7,8 @@
  * Copyright (c) Fred Heusschen
  * www.frebsite.nl
  *
- * Dual licensed under the MIT and GPL licenses.
+ * Licensed under the MIT license.
  * http://en.wikipedia.org/wiki/MIT_License
- * http://en.wikipedia.org/wiki/GNU_General_Public_License
  */
 
 
@@ -17,7 +16,7 @@
 
 	var _PLUGIN_	= 'respontent',
 		_ABBR_		= 'respontent',
-		_VERSION_	= '1.0.0';
+		_VERSION_	= '1.2.0';
 
 
 	//	Plugin already excists
@@ -28,7 +27,7 @@
 
 
 	//	Global variables
-	var _c = {}, _d = {}, _e = {},
+	var _c = {},
 		_pluginInitiated = false;
 
 
@@ -40,85 +39,38 @@
 		this.$wrapper	= $wrapper;
 		this.opts  		= opts;
 
-		if ( this.opts.images )
+		for ( var a = 0; a < $[ _PLUGIN_ ].media.length; a++ )
 		{
-			this.images();
-		}
-		if ( this.opts.youtube )
-		{
-			this.youtube();
-		}
-		if ( this.opts.googlemaps )
-		{
-			this.googlemaps();
-		}
-		if ( this.opts.tables )
-		{
-			this.tables();
+			if ( typeof this[ 'fit_' + $[ _PLUGIN_ ].media[ a ] ] == 'function' )
+			{
+				this[ 'fit_' + $[ _PLUGIN_ ].media[ a ] ]();
+			}
 		}
 
 		return this;
 	};
+
 	$[ _PLUGIN_ ].prototype = {
 
-		images: function()
-		{
-			this.$wrapper
-				.find( 'img' )
-				.addClass( _c[ 'image' ] );
-		},
-		youtube: function()
-		{
-			this.$wrapper
-		    	.find( 'iframe, object, embed' )
-		    	.filter( '[src*="youtube.com"], [src*="youtube-nocookie.com"]' )
-		    	.each(
-		        	function()
-					{
-		        	    wrapInParent( $(this), _c[ 'youtube' ] );
-					}
-				);
-		},
-		googlemaps: function()
-		{
-		    this.$wrapper
-		    	.find( 'iframe' )
-		    	.filter( '[src*="maps.google"]' )
-		    	.each(
-		        	function()
-					{
-		        	    wrapInParent( $(this), _c[ 'googlemaps' ] );
-					}
-				);
-		},
-		tables: function()
-		{
-			var tbl = ( this.opts.tables == 'no-styling' )
-				? ''
-				: _c[ 'table-pretty' ] + ' ';
-
-		    this.$wrapper
-		    	.find( 'table' )
-		    	.each(
-			        function()
-			        {
-			            var $t = $(this);
-			
-			            if ( !$t.find( 'tbody' ).length )
-			            {
-			                $t.wrapInner( '<tbody />' ).find( 'tbody' );
-			            }
-
-			            var cls = ( $t.find( 'th' ).length || $t.find( 'thead' ).length || $t.find( 'tfoot' ).length  )
-							? _c[ 'table-scroll' ]
-							: _c[ 'table' ];
-
-			            wrapInParent( $t, tbl + cls );
-			        }
-			    );
-		}
+		wrapInParent: function( $t, iClass )
+	    {
+	        var $p = $t.parent();
+	
+	        if ( $p.children().length == 1 )
+	        {
+	            $p.addClass( iClass );
+	        }
+	        else
+	        {
+	            $t.wrap( '<div class="' + iClass + '" />' );
+	        }
+	    }
 
 	};
+
+	$[ _PLUGIN_ ].defaults 	= {};
+	$[ _PLUGIN_ ].media 	= [];
+	$[ _PLUGIN_ ].version 	= _VERSION_;	
 
 
 	/*
@@ -129,7 +81,7 @@
 		//	First time plugin is fired
 		if ( !_pluginInitiated )
 		{
-			initiatePlugin();
+			_initiatePlugin();
 		}
 
 		//	Extend options
@@ -150,35 +102,9 @@
 
 
 	/*
-		Options
-	*/
-	$[ _PLUGIN_ ].defaults = {
-		images		: true,
-		youtube		: true,
-		googlemaps	: true,
-		tables		: true
-	};
-
-	$[ _PLUGIN_ ].version = _VERSION_;
-
-
-	/*
 		Private functions
 	*/
-    function wrapInParent( $t, iClass )
-    {
-        var $p = $t.parent();
-
-        if ( $p.children().length == 1 )
-        {
-            $p.addClass( iClass );
-        }
-        else
-        {
-            $t.wrap( '<div class="' + iClass + '" />' );
-        }
-    }
-	function initiatePlugin()
+	function _initiatePlugin()
 	{
 		_pluginInitiated = true;
 
@@ -191,7 +117,8 @@
 				_c[ c[ d ] ] = _c( c[ d ] );
 			}
 		};
-		_c.add( 'image youtube googlemaps table table-scroll table-pretty' );
-	};
+
+		$[ _PLUGIN_ ]._c = _c;
+	}
 
 })( jQuery );
